07da5329133ed306c78f75a2c58a8cf90ccc1c01
audili <a.joudad@gmail.com>
2021-11-18 15:14:36 -0500

    push recapitulatif
    push recapitulatif

---
+++ src/vue/PanneauTranslation.java
@@ -0,0 +1,28 @@
+package vue;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanneauTranslation  extends JPanel {
+    /**
+     *
+     */
+
+    public PanneauTranslation(){
+
+    }
+    @Override
+    public void paint(Graphics g) {
+
+    }
+
+    public void Translate(int x , int y ){
+
+    }
+
+    @Override
+    public Point getMousePosition()  {
+        return super.getMousePosition();
+    }
+}
---
+++ src/vue/PanneauZoom.java
@@ -0,0 +1,10 @@
+package vue;
+
+public class PanneauZoom {
+
+    public PanneauZoom (){
+
+    }
+
+    
+}
